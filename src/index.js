ed8841ab34bda1690cbe1e5405dbe6d5d8d6af05
Liudmila Lupandina <liudmila.lupandina@yandex.ru>
2022-10-16 14:32:48 +0300

    fix: исправить cardsmap

--- src/index.js
+++ src/index.js
@@ -6,10 +6,8 @@ import reportWebVitals from './reportWebVitals';
 
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-const rootElement = 
-document.getElementById('root');
-const root = 
-createRoot(rootElement);
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <App />
